4bd6082cf3ac3df70e998a0b2dac8f8683cc1487
angelo <magangde@yahoo.de>
2017-09-25 13:52:02 +0200

    name refactored

--- HowToRunTestChrome/src/main/java/com/angelo/selenium/udemy/howtoruntestchrome/LocatorsDemo1.java
+++ HowToRunTestChrome/src/main/java/com/angelo/selenium/udemy/howtoruntestchrome/LocatorsDemo1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LocationDemo1 {
+public class LocatorsDemo1 {
 
     public static void main(String[] args) {
         //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Angelo\\Downloads\\chromedriver_win32\\chromedriver.exe");
