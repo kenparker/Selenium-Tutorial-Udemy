77aa05b12227d42f71ebe3f03a2e35bc0ad908d5
angelo <magangde@yahoo.de>
2017-09-25 13:55:48 +0200

    demo3 added

---
+++ HowToRunTestChrome/src/main/java/com/angelo/selenium/udemy/howtoruntestchrome/TUMOnlineAnmeldungDemo3.java
@@ -0,0 +1,38 @@
+package com.angelo.selenium.udemy.howtoruntestchrome;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TUMOnlineAnmeldungDemo3 {
+
+    public static void main(String[] args) {
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Angelo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Maggioni\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://campusquality.tum.de/QSYSTEM_TUM/webnav.ini");
+              
+        navigateToLogin(driver);
+        doLogin(driver);
+        
+        driver.close();
+    }
+    
+    private static void navigateToLogin(WebDriver driver) {
+                
+        driver.switchTo().frame("menue");
+        driver.findElement(By.id("menue_frame_key_icon")).click();
+        
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame("detail");
+    }
+
+    private static void doLogin(WebDriver driver) {
+        driver.findElement(By.xpath("//*[@name='cp1']")).sendKeys("gu32fur");
+        driver.findElement(By.xpath("//*[@name='cp2']")).sendKeys("12TeRo34");
+        driver.findElement(By.xpath("//*[text()='Anmeldung']")).click();
+
+    }
+
+
+}
